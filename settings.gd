210e445464d33f60fd643948917079b4a93e3f8e
--get <mathis.feraut@gmail.com>
2024-08-23 16:17:05 +0200

    FIX AUDIO SLIDERS RETGREG

--- settings.gd
+++ settings.gd
@@ -2,18 +2,20 @@ extends Control
 
 func _ready():
 	if Status.level == 0: material = null
-
-func _on_close_settings_pressed():
-	hide()
-
-func _process(delta):
 	$HSlider.value = db_to_linear(AudioServer.get_bus_volume_db(AudioServer.get_bus_index("music")))
 	$HSlider2.value = db_to_linear(AudioServer.get_bus_volume_db(AudioServer.get_bus_index("sfx")))
 
 
-func _on_h_slider_drag_ended(value_changed):
-	AudioServer.set_bus_volume_db(AudioServer.get_bus_index("music"), linear_to_db($HSlider.value))
+
+func _on_close_settings_pressed():
+	hide()
+	
 
 
-func _on_h_slider_2_drag_ended(value_changed):
-	AudioServer.set_bus_volume_db(AudioServer.get_bus_index("sfx"), linear_to_db($HSlider2.value))
+func _on_h_slider_2_value_changed(value):
+	AudioServer.set_bus_volume_db(AudioServer.get_bus_index("sfx"), linear_to_db(value))
+
+
+
+func _on_h_slider_value_changed(value):
+	AudioServer.set_bus_volume_db(AudioServer.get_bus_index("music"), linear_to_db(value))
